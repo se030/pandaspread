5665269a9a618f07e378903e306c8d8e5feb170e
se030 <se030.kim@gmail.com>
2023-04-12 18:52:46 +0900

    chore: recoil 설치

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,19 @@
 import { ThemeProvider } from '@emotion/react';
 import { BrowserRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
 import Routes from '@/routes';
 import { theme } from '@/styles/theme';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
-    </ThemeProvider>
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+      </ThemeProvider>
+    </RecoilRoot>
   );
 }
 
