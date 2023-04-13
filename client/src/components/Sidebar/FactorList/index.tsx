36ac06649d18e47758a8c427797a1b46831f49f0
se030 <se030.kim@gmail.com>
2023-04-13 18:21:57 +0900

    feat: FactorList 컴포넌트
    버튼을 통해 각 factor의 디스플레이 방식과 표현 여부 컨트롤

---
+++ client/src/components/Sidebar/FactorList/index.tsx
@@ -0,0 +1,62 @@
+import { css, useTheme } from '@emotion/react';
+import { RxDragHandleDots2 } from 'react-icons/rx';
+import { useRecoilState } from 'recoil';
+
+import OptionButtons from './OptionButtons';
+
+import { dataframeAtom } from '@/store/atom/dataframe';
+import { ThemeColor } from '@/styles/theme';
+
+const FactorList = () => {
+  const [{ columns }] = useRecoilState(dataframeAtom);
+
+  const { color } = useTheme();
+
+  return (
+    <ol css={style.ol(color)}>
+      {columns?.map((el, idx) => (
+        <li key={idx}>
+          <div className="leading">
+            <button>
+              <RxDragHandleDots2 />
+            </button>
+            <span>{el}</span>
+          </div>
+          <div>
+            <OptionButtons idx={idx} />
+          </div>
+        </li>
+      ))}
+    </ol>
+  );
+};
+
+export default FactorList;
+
+const style = {
+  ol: ({ gray100, black }: ThemeColor) =>
+    css({
+      li: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: '1rem',
+        borderBottom: `1px solid ${gray100}`,
+
+        div: {
+          display: 'flex',
+          alignItems: 'center',
+          gap: '0.25rem',
+        },
+
+        'div.leading': {
+          '& > button': {
+            color: gray100,
+          },
+          '& > span': {
+            color: black,
+          },
+        },
+      },
+    }),
+};
