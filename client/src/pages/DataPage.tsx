1b84e6d827ec6b4de0b971099490d43266b1eb7d
se030 <se030.kim@gmail.com>
2023-04-17 01:14:25 +0900

    refactor: 스크롤 컨테이너 컴포넌트 분리
    ref로 관리하는 노드의 조건부 렌더링 제거

--- client/src/pages/DataPage.tsx
+++ client/src/pages/DataPage.tsx
@@ -2,53 +2,34 @@ import { css } from '@emotion/react';
 import { useRef } from 'react';
 
 import Loader from '@/components/_common/Loader';
+import DataSection from '@/components/DataSection';
 import Sidebar from '@/components/Sidebar';
-import Table from '@/components/Table';
 import { ColumnContext } from '@/contexts/ColumnContext';
-import { ScrollContext } from '@/contexts/ScrollContext';
 import { useDataframe } from '@/hooks/useDataframe';
-import { useScroll } from '@/hooks/useScroll';
 
 const DataPage = () => {
   const { dataframe } = useDataframe();
-
   const columnRefs = useRef<HTMLTableCellElement[]>([]);
 
-  const { scrollTop, ref: sectionRef } = useScroll();
-
   return (
-    <main css={style.main}>
-      <ColumnContext.Provider value={{ columnRefs }}>
-        {dataframe.columns.length ? (
-          <>
-            <Sidebar />
-            <ScrollContext.Provider value={{ scrollTop }}>
-              <section css={style.section} ref={sectionRef}>
-                {dataframe && <Table dataframe={dataframe} />}
-              </section>
-            </ScrollContext.Provider>
-          </>
-        ) : (
-          <Loader />
-        )}
-      </ColumnContext.Provider>
+    <main css={style}>
+      {dataframe.columns.length ? (
+        <ColumnContext.Provider value={{ columnRefs }}>
+          <Sidebar />
+          <DataSection />
+        </ColumnContext.Provider>
+      ) : (
+        <Loader />
+      )}
     </main>
   );
 };
 
 export default DataPage;
 
-const style = {
-  main: css({
-    display: 'flex',
-    flexDirection: 'row',
-    width: '100%',
-    height: '100%',
-  }),
-  section: css({
-    width: 'calc(100% - 250px)',
-    height: '100%',
-    padding: '0 2rem 1rem 2rem',
-    overflowY: 'scroll',
-  }),
-};
+const style = css({
+  display: 'flex',
+  flexDirection: 'row',
+  width: '100%',
+  height: '100%',
+});
