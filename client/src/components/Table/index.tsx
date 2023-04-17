d0b58847bc6d513051a6225690b2c7baa6104939
se030 <se030.kim@gmail.com>
2023-04-17 13:16:37 +0900

    feat: 테이블에 가상 스크롤 적용

--- client/src/components/Table/index.tsx
+++ client/src/components/Table/index.tsx
@@ -6,6 +6,7 @@ import Td from './Td';
 import { ColumnContext } from '@/contexts/ColumnContext';
 import { useColumnVisibility } from '@/hooks/useColumnVisibility';
 import { useSafeContext } from '@/hooks/useSafeContext';
+import useVirtualScroll from '@/hooks/useVirtualScroll';
 import { dataframeAtom } from '@/store/atom/dataframe';
 import { ThemeColor } from '@/styles/theme';
 
@@ -13,7 +14,7 @@ const Table = () => {
   const [{ columns, data }] = useRecoilState(dataframeAtom);
   const { columnVisibility } = useColumnVisibility();
 
-  const initialSlice = data.slice(0, 100); // TODO: pagination
+  const { start, offset, style: scrollStyle } = useVirtualScroll(data.length);
 
   const { columnRefs } = useSafeContext(ColumnContext);
 
@@ -22,7 +23,7 @@ const Table = () => {
   return (
     <table css={style.table(color)}>
       <thead>
-        <tr css={style.tr(columns.length)}>
+        <tr>
           <th>Index</th>
           {columns.map((col, idx) => (
             <th
@@ -35,10 +36,11 @@ const Table = () => {
           ))}
         </tr>
       </thead>
-      <tbody>
-        {initialSlice.map((row, index) => (
+      <tbody css={style.tbody}>
+        <tr css={scrollStyle.dummy(start)}></tr>
+        {data.slice(start, start + offset).map((row, index) => (
           <tr key={index}>
-            <td>{index}</td>
+            <td>{start + index}</td>
             {row.map((value, idx) => (
               <Td
                 key={`${idx}-${value}-${columnVisibility?.[idx]}`}
@@ -47,6 +49,7 @@ const Table = () => {
             ))}
           </tr>
         ))}
+        <tr css={scrollStyle.dummy(data.length - start + offset)}></tr>
       </tbody>
     </table>
   );
@@ -69,22 +72,27 @@ const style = {
       },
 
       th: {
+        '&:nth-of-type(1)': {
+          width: 'fit-content', // Index column
+        },
+
         padding: '2rem 1rem 1.5rem 1rem',
         fontWeight: 'bold',
         backgroundColor: offwhite,
       },
-      td: {
-        padding: '1rem',
-      },
     }),
-  tr: (columns: number) =>
-    css({
-      th: {
-        width: `calc(100% / ${columns})`,
 
-        '&:nth-of-type(1)': {
-          width: 'fit-content',
-        },
+  tbody: css({
+    tr: {
+      borderBottom: '1px solid #e2e2e2',
+      '&:nth-last-of-type(1)': {
+        borderBottom: 'none',
       },
-    }),
+    },
+    td: {
+      padding: '1rem',
+      textAlign: 'start',
+      whiteSpace: 'nowrap',
+    },
+  }),
 };
---
+++ client/src/constants/table-row.ts
@@ -0,0 +1,5 @@
+export const ROW = {
+  HEIGHT: 52,
+  VISIBLE: 50,
+  PADDED: 50,
+} as const;
