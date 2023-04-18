2ed557cac215e420e990070d50dab1bbec9f71f2
se030 <se030.kim@gmail.com>
2023-04-18 14:57:57 +0900

    feat: Thead 컴포넌트에 description 정보 추가

--- client/src/components/Table/Thead/index.tsx
+++ client/src/components/Table/Thead/index.tsx
@@ -1,6 +1,9 @@
 import { css, useTheme } from '@emotion/react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 
+import { getDescription } from '@/apis/dataframe-describe';
 import { ColumnContext } from '@/contexts/ColumnContext';
 import { useColumnVisibility } from '@/hooks/useColumnVisibility';
 import { useSafeContext } from '@/hooks/useSafeContext';
@@ -8,17 +11,38 @@ import { dataframeAtom } from '@/store/atom/dataframe';
 import { ThemeColor } from '@/styles/theme';
 
 const Thead = () => {
-  const [{ columns }] = useRecoilState(dataframeAtom);
+  const [{ data, columns }] = useRecoilState(dataframeAtom);
   const { columnVisibility } = useColumnVisibility();
 
   const { columnRefs } = useSafeContext(ColumnContext);
 
+  const { id } = useParams();
+  const [isDescriptionOpen, setIsDescriptionOpen] = useState(false);
+  const [descriptions, setDescriptions] = useState<Description[] | null>(null);
+
+  const loadDescriptions = async () => {
+    if (id === undefined) return;
+
+    const { data } = await getDescription(id);
+    setDescriptions(data);
+  };
+
+  useEffect(() => {
+    loadDescriptions();
+  }, [columns]);
+
   const { color } = useTheme();
 
   return (
-    <thead css={style.thead(color)}>
+    <thead
+      css={style.thead(color)}
+      onClick={() => setIsDescriptionOpen((prev) => !prev)}
+      className={isDescriptionOpen ? '' : 'hide'}
+    >
       <tr>
-        <th>Index</th>
+        <th>
+          Index<p>{data.length}</p>
+        </th>
         {columns.map((col, idx) => (
           <th
             key={col}
@@ -26,6 +50,7 @@ const Thead = () => {
             ref={(ref) => ref && columnRefs.current.push(ref)}
           >
             {col}
+            {descriptions && getDescriptionElements(descriptions[idx])}
           </th>
         ))}
       </tr>
@@ -36,12 +61,13 @@ const Thead = () => {
 export default Thead;
 
 const style = {
-  thead: ({ offwhite }: ThemeColor) =>
+  thead: ({ gray100, gray300, offwhite }: ThemeColor) =>
     css({
       position: 'sticky',
       top: 0,
       zIndex: 1,
       boxShadow: '0 1px 0 rgba(0, 0, 0, 0.1)',
+      cursor: 'pointer',
 
       th: {
         '&:nth-of-type(1)': {
@@ -51,9 +77,37 @@ const style = {
         padding: '2rem 1rem 1.5rem 1rem',
         fontWeight: 'bold',
         backgroundColor: offwhite,
+
+        p: {
+          width: 'fit-content',
+          margin: 'auto',
+          marginTop: '0.5rem',
+          whiteSpace: 'nowrap',
+
+          color: gray300,
+          fontSize: '0.75rem',
+          lineHeight: '1rem',
+          padding: '0.25rem',
+          border: `1px solid ${gray100}`,
+          borderRadius: '4px',
+
+          '&:nth-of-type(1)': {
+            color: gray300,
+          },
+        },
+      },
+
+      '&.hide': {
+        paddingBottom: 0,
+
+        p: {
+          visibility: 'hidden',
+          height: 0,
+          marginTop: 0,
+        },
       },
     }),
-  button: ({ gray100, gray300, black }: ThemeColor) =>
+  button: ({ gray100, gray300 }: ThemeColor) =>
     css({
       color: gray300,
       fontSize: '0.75rem',
@@ -61,14 +115,29 @@ const style = {
       padding: '0.25rem',
       border: `1px solid ${gray100}`,
       borderRadius: '4px',
-
-      '& > .icon': {
-        transform: 'translateY(1.5px)',
-      },
-
-      '&:hover': {
-        color: black,
-        border: `1px solid ${gray300}`,
-      },
     }),
+};
+
+const getDescriptionElements = (desc: Description) => {
+  if (desc.type === 'categorical') {
+    const categoricalDesc = desc as CategoricalDescription;
+
+    return (
+      <>
+        <p>{categoricalDesc.count}</p>
+        <p>
+          {categoricalDesc.freq} {categoricalDesc.top}
+        </p>
+      </>
+    );
+  } else {
+    const numericalDesc = desc as NumericalDescription;
+    return (
+      <>
+        <p>{numericalDesc.count}</p>
+        <p>mean {numericalDesc.mean.toFixed(2)}</p>
+        <p>std {numericalDesc.std.toFixed(2)}</p>
+      </>
+    );
+  }
 };
