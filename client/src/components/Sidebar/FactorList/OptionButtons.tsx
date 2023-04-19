7dac15986f98721531209d4ac5be8e3635d79de4
se030 <se030.kim@gmail.com>
2023-04-19 12:47:46 +0900

    feat: 오름/내림차순 및 정렬 기준 factor 표시

--- client/src/components/Sidebar/FactorList/OptionButtons.tsx
+++ client/src/components/Sidebar/FactorList/OptionButtons.tsx
@@ -1,8 +1,8 @@
 import { css, useTheme } from '@emotion/react';
-import { MouseEventHandler } from 'react';
+import { MouseEventHandler, useState } from 'react';
 import { AiOutlineClear } from 'react-icons/ai';
-import { BiSortDown, BiSortUp } from 'react-icons/bi';
 import { BsEyeFill, BsEyeSlashFill } from 'react-icons/bs';
+import { FaSortAmountUpAlt, FaSortAmountDown } from 'react-icons/fa';
 import { useParams } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 
@@ -77,29 +77,44 @@ export const SortButton = ({ column }: { column: string }) => {
   const { id } = useParams();
   const [, setDataframe] = useRecoilState(dataframeAtom);
 
+  const [isAscending, setIsAscending] = useState(false);
+
   const { color } = useTheme();
+
+  const sortDataframe = async (id: string, isAsc: boolean) => {
+    const { data } = await putSortBy(id, column, isAsc);
+    setDataframe(data);
+  };
 
   const onSort: MouseEventHandler = async (e) => {
     e.stopPropagation();
 
     if (!id) return;
+    setIsAscending((prev) => {
+      sortDataframe(id, !prev);
+      return !prev;
+    });
 
-    const { data } = await putSortBy(id, column, true);
-    setDataframe(data);
+    const { currentTarget } = e;
+    document.querySelectorAll('.sort-button').forEach((el) => {
+      if (el === currentTarget) el.classList.add('active');
+      else el.classList.remove('active');
+    });
   };
 
   return (
-    <button css={style.button(color)} onClick={onSort}>
-      {
-        // eslint-disable-next-line no-constant-condition
-        true ? <BiSortDown className="icon" /> : <BiSortUp className="icon" />
-      }
+    <button css={style.button(color)} onClick={onSort} className="sort-button">
+      {isAscending ? (
+        <FaSortAmountUpAlt className="icon" />
+      ) : (
+        <FaSortAmountDown className="icon" />
+      )}
     </button>
   );
 };
 
 const style = {
-  button: ({ gray100, gray300, black }: ThemeColor) =>
+  button: ({ primaryDark, gray100, gray300, black }: ThemeColor) =>
     css({
       color: gray300,
       fontSize: '0.75rem',
@@ -115,6 +130,10 @@ const style = {
       '&:hover': {
         color: black,
         border: `1px solid ${gray300}`,
+      },
+
+      '&.active': {
+        color: primaryDark,
       },
     }),
   text: css({
