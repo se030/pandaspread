d1073f1089661426ee969a1d4b1392f395b3873f
se030 <se030.kim@gmail.com>
2023-04-19 12:28:02 +0900

    fix: ascending params 숫자로 변경

--- client/src/apis/dataframe-sort.ts
+++ client/src/apis/dataframe-sort.ts
@@ -3,7 +3,7 @@ import { api } from './core';
 export const putSortBy: PutSortByRequest = async (id, column, ascending) => {
   const { data: res } = await api.put(`/dataframe/${id}/sort`, {
     column,
-    ascending: ascending ? 'True' : 'False',
+    ascending: ascending ? 1 : 0,
   });
   const { data } = res;
 
